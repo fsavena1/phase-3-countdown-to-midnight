77d5055da4563e0cc3ac4323f0b754974a104386
frank <franksavena@gmail.com>
2022-12-19 09:13:23 -0600

    commit tree d891bf2919c1712425abc0152f3db5e1405e6cf6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(n)
+    n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+   n -= 1
+#    sleep(1)
+  end
+     "HAPPY NEW YEAR!"
 end
+
+
+
+ def countdown_with_sleep(n)
+      n = 10
+  while n > 0
+    puts "#{n} SECOND(S)!"
+   n -= 1
+   sleep(1)
+  end
+     "HAPPY NEW YEAR!"
+ end
